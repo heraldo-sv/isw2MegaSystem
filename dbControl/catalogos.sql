6284a5fa370fca559ee5b0004a9e2441d298f50f
reicampos <heraldo.delvalle@gmail.com>
2019-03-31 21:34:51 -0600

    Log files

---
+++ dbControl/catalogos.sql
@@ -0,0 +1,13 @@
+
+INSERT INTO `catalogos` (`id`, `llave`, `codigo`, `valor`, `descripcion`, `estado`, `created_at`, `updated_at`) VALUES
+(1, 'PROGRESO', '1', 'Ingreso vehiculo', 'Descripción de etapa 1 para el ingreso de vehiculos a la empresa, módulo Proyectos.', 1, '2017-10-27 12:00:00', '2017-10-27 12:00:00'),
+(2, 'PROGRESO', '2', 'En reparación', 'Descripción de etapa 2 para la puesta en marcha de la reparación de vehiculos en la empresa, módulo Proyectos.', 1, '2017-10-27 12:00:00', '2017-10-27 12:00:00'),
+(3, 'PROGRESO', '3', 'Actividades finalizadas', 'Descripción de etapa 3 para la finalización de actividades de la empresa, módulo Proyectos.', 1, '2017-10-27 12:00:00', '2017-10-27 12:00:00'),
+(4, 'PROGRESO', '4', 'Entregado al cliente', 'Descripción de etapa 4 para indicar que los vehiculos ya fueron entregados a sus dueños, módulo Proyectos.', 1, '2017-10-27 12:00:00', '2017-10-27 12:00:00'),
+(5, 'ESTADOPROY', '1', 'En curso', 'Detalle para indicar que el estado 1 en el modulo de proyectos indica que el mismo se encuentra: \"En proceso\".', 1, '2017-10-27 12:00:00', '2017-10-27 12:00:00'),
+(6, 'ESTADOPROY', '2', 'En pausa', 'Detalle para indicar que el estado 2 en el modulo de proyectos indica que el mismo se encuentra: \"En pausa\".', 1, '2017-10-27 12:00:00', '2017-10-27 12:00:00'),
+(7, 'ESTADOPROY', '3', 'Finalizado', 'Detalle para indicar que el estado 3 en el modulo de proyectos indica que el mismo se encuentra: \"Finalizado\".', 1, '2017-10-27 12:00:00', '2017-10-27 12:00:00'),
+(8, 'ESTADOPROY', '4', 'Cancelado', 'Detalle para indicar que el estado 4 en el modulo de proyectos indica que el mismo se encuentra: \"Cancelado\".', 1, '2017-10-27 12:00:00', '2017-10-27 12:00:00'),
+(9, 'COTESTADO', '1', 'No pagado', 'Descripción para cotizaciones no pagadas.', 1, '2017-10-27 12:00:00', '2017-10-27 12:00:00'),
+(10, 'COTESTADO', '2', 'Pagado', 'Descripción para cotizaciones pagadas.', 1, '2017-10-27 12:00:00', '2017-10-27 12:00:00'),
+(11, 'COTESTADO', '3', 'Cancelada', 'Descripción para cotizaciones canceladas.', 1, '2017-10-27 12:00:00', '2017-10-27 12:00:00');
